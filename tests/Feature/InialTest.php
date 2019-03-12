b236ee6fb01b1c982f1658f5dc2ea0f8b3f0c873
Abdelouahab Djoudi <info@djoudi.net>
2019-03-12 12:45:55 +0100

    add test case

---
+++ tests/Feature/InialTest.php
@@ -0,0 +1,13 @@
+namespace Tests
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+
+
+class IniatTest extends TestCase{
+  use PHPUnit\Framework\Assert;
+
+ public function my_firest_test(){
+   $this->assertTrue(true);
+ }
+}
