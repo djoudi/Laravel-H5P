81d2b8d360b80190281ee0c6adbf89ba9a640050
Abdelouahab Djoudi <info@djoudi.net>
2019-03-12 16:24:59 +0100

    Update InialTest.php

--- tests/Feature/InialTest.php
+++ tests/Feature/InialTest.php
@@ -1,11 +1,11 @@
-namespace Tests
+<?php
+namespace Tests;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Assert;
 
 
 
 class IniatTest extends TestCase{
-  use PHPUnit\Framework\Assert;
 
  public function my_firest_test(){
    $this->assertTrue(true);
