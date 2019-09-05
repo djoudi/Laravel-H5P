10f2bb65431b25d9cda272129c6ffc17d32b9f6b
Abdelouahab Djoudi <info@djoudi.net>
2019-09-05 16:53:26 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/LaravelH5p/Http/Controllers/LibraryController.php
+++ src/LaravelH5p/Http/Controllers/LibraryController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use DB;
 use Djoudi\LaravelH5p\Eloquents\H5pContent;
 use Djoudi\LaravelH5p\Eloquents\H5pLibrary;
-use Djoudi\LaravelH5p\LaravelH5p;
 use H5PCore;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -146,6 +145,7 @@ class LibraryController extends Controller
         }
 
         Log::info('Not Good Good ');
+
         return redirect()
             ->route('h5p.library.index')
             ->with('error', trans('laravel-h5p.library.can_not_updated'));
